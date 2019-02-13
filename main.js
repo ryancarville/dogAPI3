d21f6fde20d02a83ce6bc27ed9aa8a25e7b643a8
Ryan Carville <ryancarville@gmail.com>
2019-02-13 20:24:01 +0100

    Inital Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function getDogImages(breed){
+    let search = 'https://dog.ceo/api/breed/'+ breed +'/images/random';
+    fetch(search)
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.status);
+            }
+            return response;
+            })
+            .then(response => response.json())
+            .then(responseJson => 
+                displayDogs(responseJson))
+            .catch(err => 
+                failureCallback(err))
+}
+
+function displayDogs(happy){
+    console.log(happy);
+    $('.dogResults').append(`<img src="${happy.message}">`)
+    $('.dogResults').removeClass('hidden');
+}
+ function failureCallback(unhappy) {
+    console.log(unhappy.message);
+    $('.dogResults').append(`We are sorry but your breed doesn't exsist in our image base. ${unhappy.message} Error`)
+    $('.dogResults').removeClass('hidden');
+ }
+
+function dogSearch(){
+    $('form').submit(event => {
+        event.preventDefault();
+        $('.dogResults').empty();
+        let breedOfDog = document.getElementById("breedOfDog").value;
+        getDogImages(breedOfDog);
+    });
+}
+
+$(function() {
+    console.log('Search is loaded');
+    dogSearch();
+});
+
+
